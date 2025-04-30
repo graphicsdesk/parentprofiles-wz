7ce23711fce4e3ad6df4b9eb468ef296034bf940
Wilson Zhen <wilsonzhen@Wilsons-MacBook-Pro-5.local>
2025-04-30 02:01:51 -0400

    Deploy to gh-pages

--- scrolly.880ca471.js
+++ scrolly.880ca471.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./images/cover_v4.jpg":[["cover_v4.b9bdf131.jpg","images/cover_v4.jpg"],"images/cover_v4.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./images/cover_v4.jpg":[["cover_v4.b9bdf131.jpg","images/cover_v4.jpg"],"images/cover_v4.jpg"],"./images/cover-iphone.jpg":[["cover-iphone.3579eba3.jpg","images/cover-iphone.jpg"],"images/cover-iphone.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64502" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52041" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
